8452bdd191db61a30a6c3fcb88e5047a38468a75
conwayhogan <conwayhogan@CONWAYHOGANDB94>
2024-01-17 18:30:03 -0700

    two files

---
+++ Dice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//a class for the dice
+internal class Dice
+{
+    private Random random;
+
+    //instance of random
+    public Dice()
+    {
+        random = new Random();
+    }
+
+    //function for rolling the dice and returning the array of results
+    public int[] RollUm(int numRolls)
+    {
+        int[] results = new int[13];
+
+        //loop to do it the number of times the user requests
+        for (int i = 0; i < numRolls; i++)
+        {
+            //rolls dice and adds sum
+            int dice1 = random.Next(1, 7);
+            int dice2 = random.Next(1, 7);
+            int sum = dice1 + dice2;
+
+            results[sum]++;
+        }
+
+        //returns the array
+        return results;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿//dice game that randomly simulates the rolling of two dice and prints the percent of the total 
 //that each number was using astrix 
 //conway hogan
-
 using System;
 
 //class for the game
-internal class DiceGame
+internal class Program
 {
     private static void Main()
     {
@@ -38,35 +37,4 @@ internal class DiceGame
 
     }
 
-}
-//a class for the dice
-class Dice
-{
-    private Random random;
-
-    //instance of random
-    public Dice()
-    { 
-        random = new Random();
-    }
-
-    //function for rolling the dice and returning the array of results
-    public int[] RollUm(int numRolls)
-    {
-        int[] results = new int[13];
-
-        //loop to do it the number of times the user requests
-        for (int i = 0; i < numRolls; i++)
-        {
-            //rolls dice and adds sum
-            int dice1 = random.Next(1, 7);
-            int dice2 = random.Next(1, 7);
-            int sum = dice1 + dice2;
-
-            results[sum]++;
-        }
-
-        //returns the array
-        return results;
-    }
 }
